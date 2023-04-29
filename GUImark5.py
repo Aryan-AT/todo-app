b2b79cba629bb04456d78e5a17ff820fa402992b
Aryan Thakkar <aryanthakkar80@gmail.com>
2023-04-30 03:42:12 +0530

    initial commit

--- GUImark5.py
+++ GUImark5.py
@@ -1,10 +1,10 @@
-import mark5_functions
+from mytodoapp import mark5_functions
 import PySimpleGUI as sg
 import time
 import os
 
-if not os.path.exists('todos.txt'):
-    with open("todos.txt", 'w') as file:
+if not os.path.exists('mytodoapp/todos.txt'):
+    with open("mytodoapp/todos.txt", 'w') as file:
         pass
 
 sg.theme("Black")

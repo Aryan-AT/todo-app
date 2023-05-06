cda091844fa52647e405c4660b75feee3a6ef8eb
Aryan Thakkar <aryanthakkar80@gmail.com>
2023-05-06 20:40:41 +0530

    Add description on the new columns

--- portfolio/main.py
+++ portfolio/main.py
@@ -10,7 +10,14 @@ with col1:
 with col2:
     st.title("Aryan Thakkar")
     content = """
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
     It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
     It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
     """
     st.info(content)
+
+content2 = """
+Below you can find some of the apps. I have built in python. Feel free to conatact me!"
+"""
+st.write(content2)
+
